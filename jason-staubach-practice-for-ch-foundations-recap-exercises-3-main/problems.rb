148d516c9047fb23bbf7254831cf878610a6d32b
JasonStaubach <jbstaubach@gmail.com>
2023-05-11 15:09:40 -0400

    Update recap p.3, added no_consecutive_repeats?(arr)

--- jason-staubach-practice-for-ch-foundations-recap-exercises-3-main/problems.rb
+++ jason-staubach-practice-for-ch-foundations-recap-exercises-3-main/problems.rb
@@ -5,6 +5,13 @@ def no_dupes?(arr)
     countH.keys
 end
 
-print no_dupes?([1, 1, 2, 1, 3, 2, 4])         # => [3, 4]
-print no_dupes?(['x', 'x', 'y', 'z', 'z'])     # => ['y']
-print no_dupes?([true, true, true])            # => []
+def no_consecutive_repeats?(arr)
+    (0...arr.length - 1).none? {|i| arr[i] == arr[i + 1]}
+end
+
+
+print no_consecutive_repeats?(['cat', 'dog', 'mouse', 'dog'])     # => true
+print no_consecutive_repeats?(['cat', 'dog', 'dog', 'mouse'])     # => false
+print no_consecutive_repeats?([10, 42, 3, 7, 10, 3])              # => true
+print no_consecutive_repeats?([10, 42, 3, 3, 10, 3])              # => false
+print no_consecutive_repeats?(['x'])                              # => true
